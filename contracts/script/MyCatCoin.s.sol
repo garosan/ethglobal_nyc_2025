7e2bf6a541586904bada2bbd490714612d372bce
Garo San <garosanchez27@gmail.com>
2025-08-16 15:52:12 -0400

    Use Nora to test building the ERC-20 token

---
+++ contracts/script/MyCatCoin.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/MyCatCoin.sol";
+
+contract MyCatCoinScript is Script {
+    function run() external {
+        vm.startBroadcast();
+        new MyCatCoin();
+        vm.stopBroadcast();
+    }
+}
