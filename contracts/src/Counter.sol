bff9aae430faf397913d5f7f757a7d7bbb1943a7
Garo San <garosanchez27@gmail.com>
2025-08-16 13:40:48 -0400

    First tests with Nora

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -11,4 +11,8 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function helloWorld() public pure returns (string memory) {
+        return "Hello world";
+    }
 }
